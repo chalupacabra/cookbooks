fb4a64ab50d743d5764f0f70a3df77828b9af67d
keviny22 <kevin_young@intuit.com>
2013-04-17 12:18:39 -0700

    Removed new_relic

--- new_relic/recipes/java_agent.rb
+++
@@ -1,4 +0,0 @@
-template node['new_relic']['config']['file_path'] do
-  source 'java_agent_newrelic.yml.erb'
-  variables :config_var => node['new_relic']['config']
-end
--- new_relic/recipes/ruby_agent.rb
+++
@@ -1,4 +0,0 @@
-template node['new_relic']['config']['file_path'] do
-  source 'newrelic.yml.erb'
-  variables :config_var => node['new_relic']['config']
-end
