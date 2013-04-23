de9070f481a8ef66bd64c96abb20e451148656b7
ccloes <chad_cloes@intuit.com>
2013-04-23 10:43:40 -0700

    Removed intu baseline tweaks cookbook

--- intu-baseline-tweaks/recipes/default.rb
+++
@@ -1,4 +0,0 @@
-include_recipe "intu-baseline-tweaks::umask"
-include_recipe "intu-baseline-tweaks::fix_permissions"
-include_recipe "intu-baseline-tweaks::disable_password_expiration"
-include_recipe "intu-baseline-tweaks::chef_logrotate"
